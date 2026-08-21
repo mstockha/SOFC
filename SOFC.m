90dbe75814787d1c00ca59bea9aad3130141fca4
mstockha <mstockha@umich.edu>
2026-08-20 20:51:27 -0400

    Fix SOFC flow and heat model
    This change refines the SOFC voltage/current sizing model by clarifying the voltage equation assumptions, computing per-cell current and power consistently, and converting reactant/product flow rates to mass flow using Faraday’s law. It also updates the reaction enthalpy and heat balance to use temperature-dependent steam enthalpy and track total hydrogen, vapor, air, and heat over time.

--- SOFC.m
+++ SOFC.m
@@ -1,5 +1,7 @@
 function [H2dot,vapordot,heatdot,total_H2,total_vapor,total_heat,pdens,voltagedraw,currentdraw, airdot,total_air] = SOFC(E,T,pH2,dt,min_cells)
 
+%% Estimate Voltage Equation Values
+
 % cell voltage equation (cve) coefficients
 temp = [700,750,800];           % temperature [celsius]
 res = [0.05, 0.0367, 0.0307];   % resistance  
@@ -28,42 +30,41 @@ ics_real = subs(icseq,t,T);
 i0_real = subs(i0eq,t,T);
 
 
-V0 = 1.121;
+%% Single Cell Sizing: Voltage Equation Analysis
+
+V0 = 1.121;             % my sources indicate that this should be 1.06
 R = 8.314;
-n = 1;
-F = 96485;
+n = 1;                  % why isn't this 2, since there are 2 atoms of hydrogen?
+F = 96485;          
 pH20 = 1- pH2;
 i = 0;
 V = 0;
 T = T + 273.15;
-
 ntemp = 0;
 Vtemp = 10000;
 j=1;
 
 while Vtemp > 0 
 
-i(j) = ntemp;    
-V(j) = V0 - ntemp.*res_real - 2.*R.*T./n./F .* log(1./2 .* ...
-    (ntemp./i0_real + sqrt((ntemp./i0_real).^2 +4))) + R.*T./2./F .* ...
-    log(1 - ntemp./ias_real)- R.*T./2./F .* ...
-    log(1 + pH2.*ntemp./pH20./ias_real) + R.*T./4./F ...
-    .* log(1 - ntemp./ics_real);
-Vtemp = V(j);
-ntemp = ntemp + 0.01;
-j = j +1;
+    i(j) = ntemp;    
+    V(j) = V0 - ntemp.*res_real - 2.*R.*T./n./F .* log(1./2 ...
+        .* (ntemp./i0_real + sqrt((ntemp./i0_real).^2 +4))) ...
+        + R.*T./2./F .* log(1 - ntemp./ias_real)- R.*T./2./F ...
+        .* log(1 + pH2.*ntemp./pH20./ias_real) + R.*T./4./F ...
+        .* log(1 - ntemp./ics_real);
+    Vtemp = V(j);
+    ntemp = ntemp + 0.01;
+    j = j +1;
 
 end
 
 V(V~=real(V)) = NaN;
 power = i.*V;
+
 figure(19)
 plot(i,V)
 xlabel('Current Density (A/cm^2)')
 ylabel('Voltage (V)')
-
-A = 500; % cell area in cm^2
-
 
 figure(20)
 plot(i,power,"LineWidth", 2);
@@ -73,6 +74,8 @@ ylabel("Power Density (W/cm^2)",'FontSize',13)
 
 % Use min cells to find pdens req, find current relating to that power
 % Then find, h2dot
+A = 500;            % cell area in cm^2
+
 pdens = E./min_cells./A;
 [~,k] = max(power);
 i_cut = i;
@@ -84,24 +87,50 @@ for d = 1:length(power)
     end
 end
 
-currentdraw = spline(power,i_cut,pdens);
-voltagedraw = spline(i,V,currentdraw);
+currentdraw = spline(power,i_cut,pdens);    % per-cell current density
+voltagedraw = spline(i,V,currentdraw);      % per-cell voltage draw
 
+% determine total current and power
+current = currentdraw .* A ;              % total current (amps)
+P_cell = current .* voltagedraw ./1000;   % per-cell power (kJ/s)
+P_elec = P_cell .* min_cells;             % total power (kJ/s)
 
-H2dot = 1.05*10^(-8)  .* currentdraw .* A .* min_cells; % kg/s
+% use Nernst relation to find molar hydrogen flow
+h2mol = min_cells .* current ./ (2*F); % kg/s
 
-total_H2 = sum(H2dot.*dt);
+% find molar flow for reactants and products (mol/s) from rxn equation
+h20mol = h2mol;         % moles of steam
+O2mol = h2mol./2;       % moles of oxygen 
 
-h2mol = H2dot.*1000./2.02;
-h20mol = h2mol;
-vapordot = h20mol.*18.02./1000; % kg/s
-total_vapor = sum(vapordot.*dt);
+% convert to mass flow rates (kg/s)
+H2dot = 0.002016 .* h2mol;          % hydrogen flow
+vapordot = h20mol .* 0.01802;       % steam flow
+airdot = O2mol .* 0.02896;          % oxygen flow
 
-O2mol = h2mol./2;
-O2dot = O2mol.*28.96./1000; % kg/s
-airdot = O2dot./0.06;
-total_air = sum(airdot.*dt);
+% determine total flow (kg)
+total_H2 = sum(H2dot.*dt);          % total hydrogen
+total_vapor = sum(vapordot.*dt);    % total steam
+total_air = sum(airdot.*dt);        % total air 
 
 
 %% Heat Calculation 
+
+% determine temperature dependent enthalpy of reaction based on data from 
+% “Appendix B: Thermodynamic Data.” In Fuel Cell Fundamentals. 
+%       John Wiley & Sons, Ltd, 2016.
+%       https://doi.org/10.1002/9781119191766.app2.
+Temps = 600:20:1000;        % temperature data (K)
+    % enthalpies in kJ/mol
+Enthalpies_Steam = [-231.33 -230.6 -229.87 -229.13 -228.39 -227.64 ...
+    -226.89 -226.13 -225.37 -224.60 -223.83 -223.05 -222.27 -221.48 ...
+    -220.69 -219.89 -219.09 -218.28 -217.47 -216.65 -215.83];
+% fit curve 
+slope_steam = polyfit(Temps, Enthalpies_Steam, 1);
+% determine reaction enthalpies (kJ/mol)
+RxnEnthalpy_Steam = polyval(slope_steam,T)
+
+% net reaction enthalpy calculation
+heatdot = h2mol.*RxnEnthalpy_Steam + P_elec;
+total_heat = sum(heatdot.*dt);
+
 end
