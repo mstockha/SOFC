c37e98387007d284b7de7c010bf5339be29b1072
mstockha <mstockha@umich.edu>
2026-08-30 21:03:44 -0400

    Update SOFC voltage fitting & temp experiments
    Add and refactor SOFC voltage-fitting and temperature experiment code and data. Key changes: extend curve fitting to 600–1000°C with new datasets and points, add VoltageEqtnFittingData and points, harden Veqtn with numeric safeguards and updated initial/bounds, compute temperature-dependent parameters (a, j0, jL) and polynomial fits. Refactor SOFCsize to use fitted parameters, update Nernst and voltage/power calculations, and compute minimum cell count. Fix bypass-ratio calculation in SOFCdriver. Add Experiment_Temperature scripts and improve plotting/labels. Update VoltageFittingData.mat.

--- SOFCsize.m
+++ SOFCsize.m
@@ -1,88 +1,47 @@
 function [min_cells, i, V, power] = SOFCsize(E,T,pH2,A)
-%% Estimate Voltage Equation Values
 
-% cell voltage equation (cve) coefficients - best fit based on Whale
-% Optimization Algorithm approach
+%% Estimate Voltage Equation Values
+% Model: Voltage Equation for each temperature
+    % i = current density (known input)
+    % params = [a, Ri, j0, jL]
+    % a = charge transfer coefficient 
+    % Ri = Area specific resistance
+    % j0 = exchange current density
+    % jL = limiting current density
+
+% cell voltage equation (cve) best fit coefficients
 temp = [700,750,800];           % temperature [celsius]
-res = [0.05, 0.0367, 0.0307];   % resistance  
-i0 = [0.2327, 0.38, 0.38];      % exchange current density
-ias = [2.3, 2.8397, 3.1323];    % anodic saturation density
-ics = [2.3, 2.8292, 3.1311];    % cathodic saturation density
+a_set = [0.314316089366992 0.428130999067575 0.463210831501386];
+j0_set = [0.0501096183073099 0.0366926976186371 0.0492195525169935];
+jL_set = [2.32125351949934 2.92470041660807 3.32757120645683];
 
 % linear fit for best fit parameters: cve coefficents fit as Var = f(T)
-syms t
-% anodic saturation current density
-coeff_ias = polyfit(temp,ias,1);
-iaseq = coeff_ias(1)*t + coeff_ias(2);
-% area specific ohmic resistance
-coeff_res = polyfit(temp,res,1);
-reseq = coeff_res(1)*t + coeff_res(2);
-% cathodic saturation current density
-coeff_ics = polyfit(temp,ics,1);
-icseq = coeff_ics(1)*t + coeff_ics(2);
-% exchange current density
-coeff_i0 = polyfit(temp,i0,1);
-i0eq = coeff_i0(1)*t + coeff_i0(2);
-
-% estimate values for given case based on temperature
-res_real = subs(reseq,t,T);     
-ias_real = subs(iaseq,t,T);
-ics_real = subs(icseq,t,T);
-i0_real = subs(i0eq,t,T);
+params_a = polyfit(temp, a_set, 2);     a = polyval(params_a, T);
+params_j0 = polyfit(temp, j0_set, 2);   j0 = polyval(params_j0, T);
+params_jL = polyfit(temp, jL_set, 2);   jL = polyval(params_jL, T);
+Ri = 0.1;        % dRi/dt = 0 per curve fitting
 
 
 %% Single Cell Sizing: Voltage Equation Analysis
 
 % set constants for cell voltage equation
-V0 = 1.121;             % Nernst voltage (V)
+V0 = 1.06;             % Nernst voltage (V)
 R = 8.314;              % universal gas constant (J/mol*K)
-n = 1;                  % number of charges/electrons transferred
-F = 96485;              % Faraday's constant (C/mol)
-pH20 = 1- pH2;          % partial pressure of water (atm)
-T = T + 273.15;         % convert ops temperature to K
+n = 2;                  % number of charges/electrons transferred
+F = 96485.34;              % Faraday's constant (C/mol)
 
-% initialize voltage loop
-i = 0;                  % initial current (amps)
-V = 0;                  % initial cell voltage (V)
-ntemp = 0;              % current counter for loop - counts up
-Vtemp = 10000;          % voltage counter for loop - counts down
-j=1;                    % iteration counter - counts up
+% set current density from 0 to limiting current density (A/cm^2)
+i = 0:0.01:(jL - 0.01);   
 
-% loop over current values to calculate all corresponding voltages
-while Vtemp > 0 % stop looping when losses due to current > nernst voltage
-    % set current index to ntemp counter value
-    i(j) = ntemp; 
-    % calculate voltage for current value
-    V(j) = V0 - ntemp.*res_real - 2.*R.*T./n./F .* log(1./2 ...
-        .* (ntemp./i0_real + sqrt((ntemp./i0_real).^2 +4))) ...
-        + R.*T./2./F .* log(1 - ntemp./ias_real)- R.*T./2./F ...
-        .* log(1 + pH2.*ntemp./pH20./ias_real) + R.*T./4./F ...
-        .* log(1 - ntemp./ics_real);
-    Vtemp = V(j);           % reset voltage counter with new cell voltage
-    ntemp = ntemp + 0.01;   % increase current value 0.01 amps 
-    j = j +1;               % increase iteration count for next iteration
-end
+% calculate voltage based on parameters (V)
+V = V0 - i.*Ri - (R.*T)./(a.*n.*F) .* (log(i./j0) ...
+    + (1 + a).*log(jL./abs(jL - i)));
 
-% due to square root operation, some values will be imaginary - set to NaN
-V(V~=real(V)) = NaN;
-
-% calculate cell power
+% calculate cell power (W/cm^2)
 power = i.*V;
 
 % maximum available power density, according to voltage relation
 pdens_max = max(power);
-
-% verify model - output i-V and i-P curves
-figure(101)
-plot(i,V)
-xlabel('Current Density (A/cm^2)')
-ylabel('Voltage (V)')
-
-figure(102)
-plot(i,power,"LineWidth", 2);
-ylim([0 1.2]);
-xlabel("Current Density (A/cm^2)",'FontSize',13)
-ylabel("Power Density (W/cm^2)",'FontSize',13)
 
 
 %% Determine minimum cell # from maximum power required
@@ -90,5 +49,21 @@ ylabel("Power Density (W/cm^2)",'FontSize',13)
 E_max = max(E);                     % max experimental power required (W)
 min_cells = E_max/(pdens_max*A);    % cell # required @ each time point
 min_cells = ceil(min_cells);        % round up to nearest whole number
+
+
+%% Verify model - output i-V and i-P curves
+
+load("VoltageFittingData.mat");     % load experimental dataset
+
+figure(101) % I-V Curve
+plot(i,V)
+xlabel('Current Density (A/cm^2)')
+ylabel('Voltage (V)')
+
+figure(102) % I-P Curve
+plot(i,power,"LineWidth", 2);
+ylim([0 1.2]);
+xlabel("Current Density (A/cm^2)",'FontSize',13)
+ylabel("Power Density (W/cm^2)",'FontSize',13)
 
 end
